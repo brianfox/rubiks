fc0da4e5fc57048f0db7c0dd27acbd8c89b188be
Brian Fox <bfox@ucsd.edu>
2011-07-30 10:02:53 -0700

    Added logback example.  Included appropriate jars in project definition.

---
+++ play/LogBackExample.java
@@ -0,0 +1,16 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+
+public class LogBackExample {
+
+  public static void main(String[] args) {
+
+    Logger logger = LoggerFactory.getLogger("chapters.introduction.HelloWorld2");
+    logger.debug("Hello world.");
+
+    LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+    StatusPrinter.print(lc);
+  }
+}
